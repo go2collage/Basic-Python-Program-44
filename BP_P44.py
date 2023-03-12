e468e2ad47ab7c452e4b3d403ed3948b092fa6df
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-12 15:01:37 +0530

    Add files via upload

---
+++ BP_P44.py
@@ -0,0 +1,11 @@
+# Python Module
+
+# import Module
+
+import Python_module
+
+college = Python_module.College_Details("Savitribai Phule Pune University", "Pune")
+
+college.show()      # it print the college name and address
+
+# Thanks for Watching
